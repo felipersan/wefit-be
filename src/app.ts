77a76864aca16a08260a2713bd97de8ed24087b7
Felipe Sanfilippo <feliperodrigues.san@gmail.com>
2023-06-21 10:46:48 -0300

    refactor: :fire: remove connect-flash dependencies

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express, { Request, Response } from "express";
 import session from 'express-session'
 import cors from "cors";
-import flash from 'connect-flash'
 
 const app = express();
 
@@ -15,26 +14,16 @@ app.use(
   })
 )
 
-app.use(flash())
 app.use(cors());
 
-app.use((req: any, res: any, next) => {
-  res.locals.success_msg = req.flash('success_msg')
-  res.locals.error_msg = req.flash('error_msg')
-  next()
-})
-
-// importação de rotas
 import userRoutes from "./routes/user.routes";
 import bodyParser from "body-parser";
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-
-//rotas
 app.use("/user", userRoutes);
 
 app.listen(PORT, () => {
-  console.log(`servidor rodando na porta ${PORT}`);
+  console.info('server running')
 });
