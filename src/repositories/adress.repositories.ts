94bb1e56e423d2bd87a4b32d8e09cd7d91231256
Felipe Sanfilippo <feliperodrigues.san@gmail.com>
2023-06-21 11:39:44 -0300

    refactor: :safety_vest: remove parsei int

--- src/repositories/adress.repositories.ts
+++ src/repositories/adress.repositories.ts
@@ -6,7 +6,7 @@ class Adress {
       data: {
         city: data.city,
         complement: data.complement ?? null,
-        houseNumber: parseInt(data.houseNumber),
+        houseNumber: data.houseNumber,
         postalCode: data.postalCode,
         state: data.state,
         street: data.street,
